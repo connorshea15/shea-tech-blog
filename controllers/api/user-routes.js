e34512bf4bb720bcfbd55e72ce02e6a76a1c656a
connorshea15 <connorshea15@gmail.com>
2020-08-27 16:41:57 -0700

    My user api routes are working swimmingly I think

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,8 +1,55 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
+// GET /api/users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => console.log(err));
+});
 
+// GET one user api/users/id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_content', 'created_at']
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'There is no user with that id!' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
+// Post /api/users to add a new user
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 
 
