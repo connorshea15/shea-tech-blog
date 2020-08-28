66b8b85d758142f46d514505fa509c5441374c92
connorshea15 <connorshea15@gmail.com>
2020-08-28 15:35:35 -0700

    Posting and getting comments

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+// route to get all comments 
+router.get('/', (req, res) => {
+    Comment.findAll({
+
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// route to create a new comment
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        user_id: req.body.user_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+
+
+module.exports = router;
