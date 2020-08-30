828ff7b1441434792307961e4d09d27d2ae1884e
connorshea15 <connorshea15@gmail.com>
2020-08-30 13:53:08 -0700

    I am submitting comments from the front end!

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,4 +38,44 @@ router.get('/login', (req, res) => {
     res.render('login');
 })
 
+// This is the route to render the single-post page
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'There is no post with this id!' });
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
 module.exports = router;
