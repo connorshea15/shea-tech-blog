cceaa54464144cafbbc48ffd548da6a4c6c0ffd7
connorshea15 <connorshea15@gmail.com>
2020-08-30 13:08:24 -0700

    I am able to logout as well!

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,6 +31,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    } 
     res.render('login');
 })
 
